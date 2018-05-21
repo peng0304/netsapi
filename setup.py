73eed9c957f1407da5d9b1c29c267b4677282690
sekou <sekou@sekous-MBP>
2018-05-21 12:41:34 +0300

    email

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='netsapi',
       version='1.0',
       description='Python front end for API',
       author='Oliver E. Bent, Sekou L. Remy',
-      author_email='hardmaru@gmail.com',
+      author_email='n/a@gmail.com',
       url='https://github.com/slremy/netsapi',
       py_modules=['location', 'environment', 'register', 'visualisation'],
 )
