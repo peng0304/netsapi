d93c258787ae3c307c4f82c391ed0afa234e0805
sekou <sekou@sekous-MBP>
2018-05-26 08:32:16 +0300

    making the poll time user defined in post

--- netsapi/environment.py
+++ netsapi/environment.py
@@ -25,7 +25,7 @@ def initEnv(locationID, userID, baseuri):
         raise e
     return envID
 
-def postAction(envID, action, baseuri):
+def postAction(envID, action, baseuri, pollingInterval = pollingInterval_seconds):
     actionUrl = '%s/api/action/v0/create'%baseuri
     reward = -10^12
     ITN_a = str(action[0]);
@@ -49,7 +49,7 @@ def postAction(envID, action, baseuri):
             #print(message)
             if message == "You have another task in queue with the same environment id":
                 env = message.split()[17]
-                reward = getReward(env, baseuri)
+                reward = getReward(env, baseuri, pollingInterval)
             else:
                 raise RuntimeError(message)
     except Exception as e:
