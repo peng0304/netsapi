9917601f985c0208e25a7e3f5d866cc70d84dc53
sekou <sekou@sekous-MBP>
2018-07-31 10:44:46 +0300

    adding code for new endpoints

--- netsapi/environment.py
+++ netsapi/environment.py
@@ -57,6 +57,32 @@ def postAction(envID, action, baseuri, pollingInterval = pollingInterval_seconds
         value = float('nan')
     return reward
 
+def postActionV1(expID, locationId, userId, action, baseuri, pollingInterval = pollingInterval_seconds):
+    actionUrl = '%s/api/v1/experiments/postJob'%baseuri
+    reward = -10^12
+    ITN_a = str(action[0]);
+    IRS_a = str(action[1]);
+    try:
+       ITN_time = "%d"%(action[2]*18+6);
+    except:
+       ITN_time = "1";
+    actions = json.dumps({"actions":[{"coverage":ITN_a, "modelName":"ITN", "time":"%st"%ITN_time},{"coverage": IRS_a,"modelName": "IRS","time":"1t"}],"experimentid":expID, "locationId":locationId , "userId":userId });
+    try:
+        response = requests.post(actionUrl, data = actions, headers = {'Content-Type': 'application/json', 'Accept': 'application/json'});
+        data = response.json();
+        print(data);
+        # print(data['statusCode'])
+        if data['statusCode'] == 202:
+            reward = getRewardV1(data['jobId'], baseuri, pollingInterval)
+        else:
+            message = data['message']
+            raise RuntimeError(message)
+    except Exception as e:
+        print(e);
+        value = float('nan')
+    return reward
+
+
 def getReward(envID, baseuri, pollingInterval = pollingInterval_seconds ):
     rewardUrl = "%s/api/action/v0/reward/%s"%(baseuri,envID)
     counter = 20;
@@ -72,8 +98,35 @@ def getReward(envID, baseuri, pollingInterval = pollingInterval_seconds ):
         value = float('nan')
     return value
 
+def getRewardV1(expID, baseuri, pollingInterval = pollingInterval_seconds):
+    rewardUrl = "%s/api/v1/experiments/reward/%s"%(baseuri,expID)
+    counter = 20;
+    try:
+        while getStatusV1(expID, baseuri) != "true" and counter > 0:
+            print (expID, getStatusV1(expID, baseuri))
+            counter -= 1
+            time.sleep(pollingInterval+random.randint(0,60));
+        reward = requests.post(rewardUrl, headers = {'Content-Type': 'application/json', 'Accept': 'application/json'})
+        #print('Cost Per Daly Averted:',reward.text)
+        value = float(reward.text) if counter > 0 else float('nan')
+    except Exception as e:
+        print(e);
+        value = float('nan')
+    return value
+
 def getStatus(envID, baseuri):
     statusUrl = "%s/api/action/v0/status/%s"%(baseuri,envID)
+    ret = "_false"
+    try:
+        response = requests.get(statusUrl, headers = {'Accept': 'application/json'})
+        ret = response.text
+    except Exception as e:
+        ret = "false %s"%(e)
+    
+    return ret
+
+def getStatusV1(expID, baseuri):
+    statusUrl = "%s/api/v1/experiments/status/%s"%(baseuri,expID)
     ret = "_false"
     try:
         response = requests.get(statusUrl, headers = {'Accept': 'application/json'})
